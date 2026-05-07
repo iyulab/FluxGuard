93da694de9213742474ca28b8e3424d58e2f18ea
zizixmas <zizixmas@gmail.com>
2026-05-07 10:29:22 +0900

    fix(test): handle null types from GetTypes() on macOS (.NET ReflectionTypeLoadException)

--- tests/FluxGuard.Tests/LogLanguageConventionTests.cs
+++ tests/FluxGuard.Tests/LogLanguageConventionTests.cs
@@ -17,7 +17,7 @@ public class LogLanguageConventionTests
     public static IEnumerable<object[]> FluxGuardAssemblyTypes()
     {
         var assembly = typeof(FluxGuardOptions).Assembly;
-        foreach (var type in assembly.GetTypes().Where(t => !t.IsCompilerGenerated()))
+        foreach (var type in SafeGetTypes(assembly).Where(t => !t.IsCompilerGenerated()))
         {
             yield return new object[] { type };
         }
@@ -26,10 +26,16 @@ public class LogLanguageConventionTests
     public static IEnumerable<object[]> FluxGuardSdkAssemblyTypes()
     {
         var assembly = typeof(FluxGuardChatClient).Assembly;
-        foreach (var type in assembly.GetTypes().Where(t => !t.IsCompilerGenerated()))
+        foreach (var type in SafeGetTypes(assembly).Where(t => !t.IsCompilerGenerated()))
         {
             yield return new object[] { type };
         }
+    }
+
+    private static IEnumerable<Type> SafeGetTypes(Assembly assembly)
+    {
+        try { return assembly.GetTypes(); }
+        catch (ReflectionTypeLoadException e) { return e.Types.Where(t => t is not null)!; }
     }
 
     [Theory]
